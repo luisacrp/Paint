7f1fffa0c73750a67560e0758b0d02c6793b0a3c
sayranufs <sayran.santana@dcomp.ufs.br>
2026-07-01 23:33:43 -0300

    Adicao de nova figura: Poligonos

--- figuras.py
+++ figuras.py
@@ -1,3 +1,5 @@
+import math
+
 # Superclasse
 class Figura: 
   def __init__(self, coordenadas, cor_borda, cor_preenchimento = ""):
@@ -59,3 +61,31 @@ class Circulo(Figura):
             canvas.create_oval(self.coordenadas[0], self.coordenadas[1], x_final, y_final, outline=self.cor_borda, fill=self.cor_preenchimento, dash=tracejado, tags=tag)
         else:
             canvas.create_oval(self.coordenadas[0], self.coordenadas[1], x_final, y_final, outline=self.cor_borda, dash=tracejado, tags=tag)
+
+
+class Poligono(Figura):
+    def __init__(self, coordenadas, cor_borda, cor_preenchimento="", num_lados=6):
+        super().__init__(coordenadas, cor_borda, cor_preenchimento)
+        self.num_lados = num_lados
+
+    def desenhar(self, canvas, tag=""):
+        tracejado = super().desenhar(canvas, tag)
+
+        x1, y1, x2, y2 = self.coordenadas
+
+        cx = (x1 + x2) / 2
+        cy = (y1 + y2) / 2
+        rx = abs(x2 - x1) / 2
+        ry = abs(y2 - y1) / 2
+
+        pontos = []
+        for i in range(self.num_lados):
+            angulo = -math.pi / 2 + (2 * math.pi * i) / self.num_lados
+            px = cx + rx * math.cos(angulo)
+            py = cy + ry * math.sin(angulo)
+            pontos.extend([px, py])
+
+        if self.cor_preenchimento != "":
+            canvas.create_polygon(pontos, outline=self.cor_borda, fill=self.cor_preenchimento, dash=tracejado, tags=tag)
+        else:
+            canvas.create_polygon(pontos, outline=self.cor_borda, fill="", dash=tracejado, tags=tag)
