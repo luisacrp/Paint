dd8eb92a97936c39e597d9006a74e600a2ab0d7c
Luísa Costa <90010999+luisacrp@users.noreply.github.com>
2026-07-13 22:25:19 -0300

    Implementação da persistência de dados (Salvar e Abrir)
    O Model foi atualizado utilizando a biblioteca nativa "pickle" para serializar e desserializar a lista de objetos, garantindo que o estado completo do desenho seja preservado e recuperado corretamente.

--- src/lss_paint/model/desenho.py
+++ src/lss_paint/model/desenho.py
@@ -1,3 +1,6 @@
+import pickle
+from src.lss_paint.model.figuras import Linha, Rabisco, Retangulo, Oval, Circulo, Poligono
+
 class DesenhoModel:
     def __init__(self):
         self.figuras = []
@@ -7,3 +10,11 @@ class DesenhoModel:
         self.cor_atual_preenchimento = "" 
         self.cor_alvo = "borda"
         self.num_lados_poligono = 6
+
+    def salvar_arquivo(self, caminho):
+        with open(caminho, 'wb') as f:
+            pickle.dump(self.figuras, f)
+
+    def carregar_arquivo(self, caminho):
+        with open(caminho, 'rb') as f:
+            self.figuras = pickle.load(f)
